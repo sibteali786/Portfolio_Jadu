d82f01c274eb2ec2643a8243921f7d199ee96bbd
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-27 20:14:56 +0500

    initialized Repo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Stick No Bills"],
+    },
+    extend: {
+      colors: {
+        primary: "#003049",
+        buttonColor: "#F77F00",
+        fontColor: "#EAE2B7",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
